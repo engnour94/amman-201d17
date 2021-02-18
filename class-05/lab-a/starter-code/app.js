b2e99afb9379c308bb49b75a569fb135a5ce1213
engnour94 <nour.abuelenein@hotmail.com>
2021-02-19 00:52:08 +0200

    solving problem 4

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -53,10 +53,10 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
     let sumResult = sum(sum(a,b)[0],c)[0];
-let productResult = multiply(multiply(a,b)[0],c)[0];
+let multiplyResult = multiply(multiply(a,b)[0],c)[0];
 let statement1 =`${a} and ${b} and ${c} sum to ${sumResult}.`;
-let statement2 = `The product of ${a} and ${b} and ${c} is ${productResult}.`;
-return[sumResult, productResult, statement1, statement2];
+let statement2 = `The product of ${a} and ${b} and ${c} is ${multiplyResult}.`;
+return[sumResult, multiplyResult, statement1, statement2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -78,12 +78,15 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    let result = sum(sum(testArray[0],testArray[1])[0],testArray[2])[0];
+    let statement = `${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${result} is their sum.`;
+    return[result,statement]
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
